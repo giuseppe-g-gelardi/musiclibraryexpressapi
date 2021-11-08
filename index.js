7f1be4b522ae2ea4e5d2cf458a95a7d40dcc6929
n-rdy <giuseppegelardi@icloud.com>
2021-11-08 11:59:34 -0600

    add get requests and post, both varified working through postman

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require('express')
+const repoContext = require('./repository/repository-wrapper')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.listen(3000, () => {
-  console.log('Server started. Listening on port 3000')
+
+app.listen(3800, () => {
+  console.log('Server started. Listening on port 3800')
+})
+
+// gets the full dataset
+app.get('/api/products', (req, res) => {
+  const products = repoContext.products.findAllProducts()
+  return res.send(products)
+})
+
+// gets single item by ID
+app.get('/api/products/:id', (req, res) => {
+  const id = req.params.id
+  const product = repoContext.products.findProductById(id)
+  return res.send(product)
+})
+
+app.post('/api/products', (req, res) => {
+  const newProduct = req.body
+  const addedProduct = repoContext.products.createProduct(newProduct)
+  return res.send(addedProduct)
 })
